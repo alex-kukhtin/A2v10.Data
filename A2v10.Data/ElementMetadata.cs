53d533e3e91d8a6d52a19123c1d67fbfa3b37c60
Александр Кухтин <alex.kukhtin@outlook.com>
2018-02-26 20:27:19 +0200

    26 feb 2018 20:27

--- A2v10.Data/ElementMetadata.cs
+++ A2v10.Data/ElementMetadata.cs
@@ -6,90 +6,90 @@ using System.Collections.Generic;
 
 namespace A2v10.Data
 {
-    public class ElementMetadata : IDataMetadata
-    {
-        IDictionary<String, IDataFieldMetadata> _fields = new Dictionary<String, IDataFieldMetadata>();
+	public class ElementMetadata : IDataMetadata
+	{
+		IDictionary<String, IDataFieldMetadata> _fields = new Dictionary<String, IDataFieldMetadata>();
 
-        public String Id { get; private set; }
-        public String Name { get; private set; }
-        public String RowNumber { get; private set; }
-        public String HasChildren { get; private set; }
-        public String Permissions { get; set; }
-        public String Items { get; set; }
+		public String Id { get; private set; }
+		public String Name { get; private set; }
+		public String RowNumber { get; private set; }
+		public String HasChildren { get; private set; }
+		public String Permissions { get; set; }
+		public String Items { get; set; }
 
-        public Boolean IsArrayType { get; set; }
-        public Boolean IsRowCount { get; set; }
-        public Boolean IsGroup { get; set; }
+		public Boolean IsArrayType { get; set; }
+		public Boolean IsRowCount { get; set; }
+		public Boolean IsGroup { get; set; }
 
-        public SortedList<String, Tuple<Int32, String>> Groups { get; private set; }
+		public SortedList<String, Tuple<Int32, String>> Groups { get; private set; }
 
-        public IDictionary<String, IDataFieldMetadata> Fields { get { return _fields; } }
+		public IDictionary<String, IDataFieldMetadata> Fields { get { return _fields; } }
 
-        public FieldMetadata AddField(FieldInfo field, DataType type)
-        {
-            if (!field.IsVisible)
-                return null;
-            FieldMetadata fm;
-            if (IsFieldExists(field.PropertyName, type, out fm))
-                return fm;
-            fm = new FieldMetadata(field, type);
-            _fields.Add(field.PropertyName, fm);
-            switch (field.SpecType)
-            {
-                case SpecType.Id:
-                    Id = field.PropertyName;
-                    break;
-                case SpecType.Name:
-                    Name = field.PropertyName;
-                    break;
-                case SpecType.RowNumber:
-                    RowNumber = field.PropertyName;
-                    break;
-                case SpecType.RowCount:
-                    IsRowCount = true;
-                    break;
-                case SpecType.HasChildren:
-                    HasChildren = field.PropertyName;
-                    break;
-                case SpecType.Permissions:
-                    Permissions = field.PropertyName;
-                    break;
-                case SpecType.Items:
-                    Items = field.PropertyName;
-                    break;
-            }
-            return fm;
-        }
+		public FieldMetadata AddField(FieldInfo field, DataType type)
+		{
+			if (!field.IsVisible)
+				return null;
+			FieldMetadata fm;
+			if (IsFieldExists(field.PropertyName, type, out fm))
+				return fm;
+			fm = new FieldMetadata(field, type);
+			_fields.Add(field.PropertyName, fm);
+			switch (field.SpecType)
+			{
+				case SpecType.Id:
+					Id = field.PropertyName;
+					break;
+				case SpecType.Name:
+					Name = field.PropertyName;
+					break;
+				case SpecType.RowNumber:
+					RowNumber = field.PropertyName;
+					break;
+				case SpecType.RowCount:
+					IsRowCount = true;
+					break;
+				case SpecType.HasChildren:
+					HasChildren = field.PropertyName;
+					break;
+				case SpecType.Permissions:
+					Permissions = field.PropertyName;
+					break;
+				case SpecType.Items:
+					Items = field.PropertyName;
+					break;
+			}
+			return fm;
+		}
 
-        public Int32 FieldCount { get { return _fields.Count; } }
+		public Int32 FieldCount { get { return _fields.Count; } }
 
-        public Boolean ContainsField(String field)
-        {
-            return _fields.ContainsKey(field);
-        }
+		public Boolean ContainsField(String field)
+		{
+			return _fields.ContainsKey(field);
+		}
 
-        bool IsFieldExists(String name, DataType dataType, out FieldMetadata fm)
-        {
-            fm = null;
-            IDataFieldMetadata ifm;
-            if (_fields.TryGetValue(name, out ifm))
-            {
-                fm = ifm as FieldMetadata;
-                if (fm.DataType != dataType)
-                    throw new DataLoaderException($"Invalid property '{name}'. Type mismatch.");
-                return true;
-            }
-            return false;
-        }
+		bool IsFieldExists(String name, DataType dataType, out FieldMetadata fm)
+		{
+			fm = null;
+			IDataFieldMetadata ifm;
+			if (_fields.TryGetValue(name, out ifm))
+			{
+				fm = ifm as FieldMetadata;
+				if (fm.DataType != dataType)
+					throw new DataLoaderException($"Invalid property '{name}'. Type mismatch.");
+				return true;
+			}
+			return false;
+		}
 
-        public FieldMetadata GetField(String name)
-        {
-            IDataFieldMetadata fm;
-            if (_fields.TryGetValue(name, out fm))
-            {
-                return fm as FieldMetadata;
-            }
-            throw new DataLoaderException($"Field '{name}' not found.");
-        }
-    }
+		public FieldMetadata GetField(String name)
+		{
+			IDataFieldMetadata fm;
+			if (_fields.TryGetValue(name, out fm))
+			{
+				return fm as FieldMetadata;
+			}
+			throw new DataLoaderException($"Field '{name}' not found.");
+		}
+	}
 }
--- A2v10.Data/FieldMetadata.cs
+++ A2v10.Data/FieldMetadata.cs
@@ -5,81 +5,83 @@ using System;
 
 namespace A2v10.Data
 {
-    public enum DataType
-    {
-        Undefined,
-        String,
-        Number,
-        Date,
-        Boolean,
-    }
+	public enum DataType
+	{
+		Undefined,
+		String,
+		Number,
+		Date,
+		Boolean,
+	}
 
-    public enum FieldType
-    {
-        Scalar,
-        Object,
-        Array,
-        Map,
-        Tree,
-        Group
-    }
+	public enum FieldType
+	{
+		Scalar,
+		Object,
+		Array,
+		Map,
+		Tree,
+		Group
+	}
 
-    public enum SpecType
-    {
-        Unknown,
-        Id,
-        Name,
-        RefId,
-        ParentId,
-        RowCount,
-        RowNumber,
-        HasChildren,
-        Items,
-        Permissions,
-        GroupMarker
-    }
+	public enum SpecType
+	{
+		Unknown,
+		Id,
+		Name,
+		RefId,
+		ParentId,
+		RowCount,
+		RowNumber,
+		HasChildren,
+		Items,
+		Permissions,
+		GroupMarker,
+		SortOrder,
+		Direction
+	}
 
-    public class FieldMetadata : IDataFieldMetadata
-    {
-        public DataType DataType { get; }
-        public FieldType ItemType { get; } // for object, array
-        public String RefObject { get; } // for object, array
-        public Boolean IsLazy { get; }
+	public class FieldMetadata : IDataFieldMetadata
+	{
+		public DataType DataType { get; }
+		public FieldType ItemType { get; } // for object, array
+		public String RefObject { get; } // for object, array
+		public Boolean IsLazy { get; }
 
-        public Boolean IsArrayLike { get { return ItemType == FieldType.Object || ItemType == FieldType.Array; } }
+		public Boolean IsArrayLike { get { return ItemType == FieldType.Object || ItemType == FieldType.Array; } }
 
-        public FieldMetadata(FieldInfo fi, DataType type)
-        {
-            DataType = type;
-            IsLazy = fi.IsLazy;
-            ItemType = FieldType.Scalar;
-            RefObject = null;
-            if (fi.IsObjectLike)
-            {
-                ItemType = fi.FieldType;
-                RefObject = fi.TypeName;
-            }
-            else if (fi.IsRefId)
-            {
-                ItemType = FieldType.Object;
-                RefObject = fi.TypeName;
-            }
-        }
+		public FieldMetadata(FieldInfo fi, DataType type)
+		{
+			DataType = type;
+			IsLazy = fi.IsLazy;
+			ItemType = FieldType.Scalar;
+			RefObject = null;
+			if (fi.IsObjectLike)
+			{
+				ItemType = fi.FieldType;
+				RefObject = fi.TypeName;
+			}
+			else if (fi.IsRefId)
+			{
+				ItemType = FieldType.Object;
+				RefObject = fi.TypeName;
+			}
+		}
 
-        public String GetObjectType(String fieldName)
-        {
-            if (ItemType == FieldType.Array)
-                return RefObject + "Array";
-            else if (ItemType == FieldType.Tree)
-                return RefObject + "Array";
-            else if (ItemType == FieldType.Object)
-                return RefObject;
-            else if (ItemType == FieldType.Group)
-                return RefObject;
-            else if (DataType == DataType.Undefined)
-                throw new DataLoaderException($"Invalid data type for 'filedName'");
-            else
-                return DataType.ToString();
-        }
-    }
+		public String GetObjectType(String fieldName)
+		{
+			if (ItemType == FieldType.Array)
+				return RefObject + "Array";
+			else if (ItemType == FieldType.Tree)
+				return RefObject + "Array";
+			else if (ItemType == FieldType.Object)
+				return RefObject;
+			else if (ItemType == FieldType.Group)
+				return RefObject;
+			else if (DataType == DataType.Undefined)
+				throw new DataLoaderException($"Invalid data type for 'filedName'");
+			else
+				return DataType.ToString();
+		}
+	}
 }
