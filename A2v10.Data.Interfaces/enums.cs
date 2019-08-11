276805fa7d0e08fadacc66d5525613842fbc1cbf
Аlex Kukhtin <alex.kukhtin@outlook.com>
2019-08-11 13:46:58 +0300

    v 7200

---
+++ A2v10.Data.Interfaces/enums.cs
@@ -0,0 +1,16 @@
+﻿// Copyright © 2018 Alex Kukhtin. All rights reserved.
+
+using System;
+
+namespace A2v10.Data.Interfaces
+{
+	[Flags]
+	public enum StdPermissions
+	{
+		None = 0,
+		CanView = 0x01,
+		CanEdit = 0x02,
+		CanDelete = 0x04,
+		CanApply = 0x08
+	}
+}
