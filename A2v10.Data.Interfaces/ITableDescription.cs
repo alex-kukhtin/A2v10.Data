c2d723f8f7784bd2312deb1d8a5d57382e664b0d
alex-kukhtin <alex.kukhtin@outlook.com>
2023-02-12 09:24:10 +0200

    v 7919

--- A2v10.Data.Interfaces/ITableDescription.cs
+++ A2v10.Data.Interfaces/ITableDescription.cs
@@ -1,14 +1,14 @@
-﻿
+﻿// Copyright © 2015-2023 Oleksandr Kukhtin. All rights reserved.
+
 using System;
 using System.Dynamic;
 
-namespace A2v10.Data.Interfaces
+namespace A2v10.Data.Interfaces;
+
+public interface ITableDescription
 {
-	public interface ITableDescription
-	{
-		IFormatProvider FormatProvider { get; set; }
-		ExpandoObject NewRow();
-		void SetValue(ExpandoObject obj, String propName, Object value);
-		ExpandoObject ToObject();
-	}
+	IFormatProvider FormatProvider { get; set; }
+	ExpandoObject NewRow();
+	void SetValue(ExpandoObject obj, String propName, Object value);
+	ExpandoObject ToObject();
 }
