218198f255f0198ad7390ac965d17de5fbf1d52c
alex-kukhtin <alex.kukhtin@outlook.com>
2023-03-03 09:20:21 +0200

    data grid css fix

---
+++ A2v10.Data.Tests/Models/DynamicGrouping.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Threading.Tasks;
+
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+using Newtonsoft.Json;
+
+using A2v10.Data.Interfaces;
+using A2v10.Data.Tests.Configuration;
+using A2v10.Data.ScriptBuilder;
+using A2v10.Data.Tests;
+
+namespace A2v10.Data.Models;
+
+[TestClass]
+[TestCategory("Models.Dynamic Grouping")]
+public class DynamicGrouping
+{
+	private readonly IDbContext _dbContext;
+	public DynamicGrouping()
+	{
+		_dbContext = Starter.Create();
+	}
+
+	[TestMethod]
+	public async Task LoadDynamicGroups()
+	{
+		var dm = await _dbContext.LoadModelAsync(null, "a2test.[DynamicGrouping]");
+	}
+
+}
